2b30cd5d3d45960c8951bc4595037556e83b1663
karima <erremytykarima@gmail.com>
2025-11-29 13:39:56 +0100

    Fix flake8 errors

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,18 +1,30 @@
-from sklearn import metrics
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+)
 
 
 def evaluate_model(y_true, y_pred_binary):
+    """
+    Évalue le modèle avec plusieurs métriques.
+    """
     return {
-        "accuracy": metrics.accuracy_score(
-            y_true, y_pred_binary
+        "accuracy": accuracy_score(
+            y_true,
+            y_pred_binary
         ),
-        "precision": metrics.precision_score(
-            y_true, y_pred_binary
+        "precision": precision_score(
+            y_true,
+            y_pred_binary
         ),
-        "recall": metrics.recall_score(
-            y_true, y_pred_binary
+        "recall": recall_score(
+            y_true,
+            y_pred_binary
         ),
-        "f1_score": metrics.f1_score(
-            y_true, y_pred_binary
+        "f1_score": f1_score(
+            y_true,
+            y_pred_binary
         ),
     }
