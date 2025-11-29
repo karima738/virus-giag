0bb171148affbb301b4010599805ff6ab5845201
karima <erremytykarima@gmail.com>
2025-11-29 13:32:12 +0100

    Fix flake8 line length in metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,12 +1,18 @@
 from sklearn import metrics
 
 
-
 def evaluate_model(y_true, y_pred_binary):
     return {
-        "accuracy": metrics.accuracy_score(y_true, y_pred_binary),
-        "precision": metrics.precision_score(y_true, y_pred_binary),
-        "recall": metrics.recall_score(y_true, y_pred_binary),
-        "f1_score": metrics.f1_score(y_true, y_pred_binary),
+        "accuracy": metrics.accuracy_score(
+            y_true, y_pred_binary
+        ),
+        "precision": metrics.precision_score(
+            y_true, y_pred_binary
+        ),
+        "recall": metrics.recall_score(
+            y_true, y_pred_binary
+        ),
+        "f1_score": metrics.f1_score(
+            y_true, y_pred_binary
+        ),
     }
-
