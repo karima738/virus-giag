d0505a269c0caa4036a2ede4eec8ddeb4506eafa
karima <erremytykarima@gmail.com>
2025-11-29 13:27:40 +0100

    Fix flake8 E501 import too long

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,18 +1,12 @@
-from sklearn.metrics import (
-    accuracy_score,
-    precision_score,
-    recall_score,
-    f1_score
-)
+from sklearn import metrics
+
 
 
 def evaluate_model(y_true, y_pred_binary):
-    """
-    Évalue le modèle avec plusieurs métriques.
-    """
     return {
-        "accuracy": accuracy_score(y_true, y_pred_binary),
-        "precision": precision_score(y_true, y_pred_binary),
-        "recall": recall_score(y_true, y_pred_binary),
-        "f1_score": f1_score(y_true, y_pred_binary),
+        "accuracy": metrics.accuracy_score(y_true, y_pred_binary),
+        "precision": metrics.precision_score(y_true, y_pred_binary),
+        "recall": metrics.recall_score(y_true, y_pred_binary),
+        "f1_score": metrics.f1_score(y_true, y_pred_binary),
     }
+
