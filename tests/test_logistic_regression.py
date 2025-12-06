b900cfca0cc197b8b4673926c80a60428cb1d04b
karima <erremytykarima@gmail.com>
2025-12-06 13:58:30 +0100

    Fix flake8 blank lines in test_logistic_regression

--- tests/test_logistic_regression.py
+++ tests/test_logistic_regression.py
@@ -1,6 +1,7 @@
 import numpy as np
 from core.logistic_regression import LogisticRegressionModel
 
+
 def test_logistic_regression_train():
     X = np.random.rand(20, 3)
     y = np.random.randint(0, 2, 20)
